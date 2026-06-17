445da317b7e9703f799fa3a5f1c44bd07b38626f
tu <lauren.silva@anima.edu.uy>
2026-06-17 17:43:57 -0300

    banner /carrusel
    banner con las promo del mes

--- Front BW/vite-project/src/assets/components/ClienteView.jsx
+++ Front BW/vite-project/src/assets/components/ClienteView.jsx
@@ -1,10 +1,47 @@
 import { useState, useEffect } from "react";
+import foto1 from "./img/1.png"; 
+import foto2 from "./img/2.png";
+import foto3 from "./img/3.png";
+
+function MiCarrusel() {
+  const imagenes = [foto1, foto2, foto3];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % imagenes.length);
+    }, 3000);
+    return () => clearInterval(intervalo);
+  }, [imagenes.length]);
+
+  const siguiente = () => setIndex((index + 1) % imagenes.length);
+  const anterior = () => setIndex((index - 1 + imagenes.length) % imagenes.length);
+
+  return (
+    <div className="custom-carousel">
+      <img src={imagenes[index]} alt={`Slide ${index + 1}`} className="carousel-img" />
+      <button onClick={anterior} className="carousel-btn prev">&#10094;</button>
+      <button onClick={siguiente} className="carousel-btn next">&#10095;</button>
+      
+      <div className="carousel-dots">
+        {imagenes.map((_, i) => (
+          <div 
+            key={i} 
+            onClick={() => setIndex(i)} 
+            className={`carousel-dot ${i === index ? "active" : ""}`} 
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default function ClienteView({ onLogout }) {
   const [books, setBooks] = useState([]);
   const [selectedBook, setSelectedBook] = useState(null);
 
   useEffect(() => {
+
     fetch("http://localhost:3000/api/books")
       .then((res) => res.json())
       .then((data) => setBooks(data))
@@ -13,11 +50,21 @@ export default function ClienteView({ onLogout }) {
       );
   }, []);
 
+
   return (
     <>
       <header className="app-header">
         <h1 className="app-title">
-          📚 Librería Índice
+           <div className="logo">
+      <img
+         src="https://i.pinimg.com/736x/9e/12/6c/9e126c7b051587c224db1b03c45239e3.jpg"
+         alt="Logo"
+         className="logo-img"
+      />
+      <h1 className="logo-text" style={{ margin: 0, marginTop: "21px" }}>
+   Libreria Indice
+</h1>
+   </div>
         </h1>
 
         <button
@@ -27,18 +74,19 @@ export default function ClienteView({ onLogout }) {
           Cerrar sesión
         </button>
       </header>
-
       <main className="main-layout">
         <section className="grid-panel">
           <div className="grid-header">
             <span className="grid-label">
               Catálogo
             </span>
-
+            
             <span className="book-count">
               {books.length} libros disponibles
             </span>
           </div>
+
+<MiCarrusel />
 
           <div className="book-grid">
             {books.map((book) => (
